1faf3506dcf11a6c73599b0bedb98aa9d1dd7c8a
Caleb Eby <caleb.eby01@gmail.com>
2018-07-16 14:09:08 -0700

    Fallback to 404 page for html files

--- sw.js
+++ sw.js
@@ -3,9 +3,14 @@ importScripts(
 );
 
 // Cache fallback for HTML files
-workbox.routing.registerRoute(
-  /(?:^[^.]+|\.html)$/,
-  workbox.strategies.networkFirst()
+workbox.routing.registerRoute(/(?:^[^.]+|\.html)$/, args =>
+  workbox.strategies
+    .networkFirst()
+    .handle(args)
+    .then(
+      // page is not in cache, respond with 404 page (even if the page exists)
+      response => (response === undefined ? caches.match('/404/') : response)
+    )
 );
 
 // Cache CSS and JS files
@@ -45,3 +50,5 @@ workbox.routing.registerRoute(
     ]
   })
 );
+
+workbox.precaching.precache(['/', '/404/', '/error/']);
