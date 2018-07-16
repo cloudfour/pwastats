a9b1d821daa6430e29bced8a5cb4d4ef4229b5f5
Caleb Eby <caleb.eby01@gmail.com>
2018-07-16 14:31:41 -0700

    Delete unrecognized caches

--- sw.js
+++ sw.js
@@ -2,6 +2,12 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js'
 );
 
+const CACHES = {
+  cssJs: 'css-js',
+  images: 'images',
+  fonts: 'google-fonts'
+};
+
 // Cache fallback for HTML files
 workbox.routing.registerRoute(/(?:^[^.]+|\.html)$/, args =>
   workbox.strategies
@@ -17,7 +23,7 @@ workbox.routing.registerRoute(/(?:^[^.]+|\.html)$/, args =>
 workbox.routing.registerRoute(
   /\.(?:js|css)$/,
   workbox.strategies.staleWhileRevalidate({
-    cacheName: 'static-resources'
+    cacheName: CACHES.cssJs
   })
 );
 
@@ -25,7 +31,7 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
   /\.(?:png|gif|jpg|jpeg|svg)$/,
   workbox.strategies.cacheFirst({
-    cacheName: 'images',
+    cacheName: CACHES.images,
     plugins: [
       new workbox.expiration.Plugin({
         maxEntries: 60,
@@ -39,7 +45,7 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
   new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
   workbox.strategies.cacheFirst({
-    cacheName: 'google-fonts',
+    cacheName: CACHES.fonts,
     plugins: [
       new workbox.expiration.Plugin({
         maxEntries: 30
@@ -52,3 +58,28 @@ workbox.routing.registerRoute(
 );
 
 workbox.precaching.precache(['/', '/404/', '/error/']);
+
+/**
+ * Returns true if the cache is one of the specified caches or starts with workbox-
+ * @param {string} cacheName
+ */
+const isCacheValid = cacheName =>
+  Object.values(CACHES).includes(cacheName) || cacheName.startsWith('workbox-');
+
+/**
+ * Delete out-of-date caches
+ */
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNameList =>
+      Promise.all(
+        cacheNameList.map(cacheName => {
+          if (!isCacheValid(cacheName)) {
+            console.info(`Deleting cache "${cacheName}"`);
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
