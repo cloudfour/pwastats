9088d2032182e7e55906002c614e4f4078971347
Emerson <emerson@cloudfour.com>
2020-06-30 14:25:01 -0700

    404 page configs

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 module.exports = function(eleventyConfig) {
 
   // aliases for backwards support of jekyll layouts
@@ -22,6 +24,23 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("manifest.json");
   eleventyConfig.addPassthroughCopy("*.svg");
   eleventyConfig.addPassthroughCopy("sw.js");
+
+  // 404 page for local server
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, bs) {
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync('_site/404.html');
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          // Add 404 http status code in request header.
+          // res.writeHead(404, { "Content-Type": "text/html" });
+          res.writeHead(404);
+          res.end();
+        });
+      }
+    }
+  });
 
   return {
     dir: {
